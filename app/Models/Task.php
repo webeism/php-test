c4380e26454c623fe9d981624c62505cfeab3288
John Hemsley <john@10degrees.uk>
2025-05-15 19:21:00 +0100

    add a task factory

--- app/Models/Task.php
+++ app/Models/Task.php
@@ -3,9 +3,11 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Task extends Model
 {
+    use HasFactory;
     protected $fillable = ['name', 'description', 'edit_token'];
     protected $hidden = ['edit_token'];
 }
---
+++ database/factories/TaskFactory.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Task>
+ */
+class TaskFactory extends Factory
+{
+
+    protected $model = \App\Models\Task::class;
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition()
+    {
+        return [
+            'name' => $this->faker->sentence(3),
+            'description' => $this->faker->paragraph(2),
+            'edit_token' => \Illuminate\Support\Str::uuid(),
+        ];
+    }
+}

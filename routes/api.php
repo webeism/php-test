0879ca3363ac319b0768cf64e319f48c3865efca
John Hemsley <john@10degrees.uk>
2025-05-15 19:08:43 +0100

    run php artisan install:api instead of manual api/php file

--- routes/api.php
+++ routes/api.php
@@ -1,7 +1,12 @@
 <?php
 
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\TaskController;
 
-Route::prefix('tasks')->group(function () {
-    Route::post('/', [TaskController::class, 'store']);
-});
+Route::get('/user', function (Request $request) {
+    return $request->user();
+})->middleware('auth:sanctum');
+
+
+Route::post('/tasks', [TaskController::class, 'store']);
